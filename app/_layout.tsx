82c9058c7c8a1fef338673f5eb4cbfb2f731aeb2
Khoa1512 <danggkhoaa1512@gmail.com>
2025-04-11 23:53:24 +0700

    feat: add root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
-import "./global.css";
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import './global.css'
+import React from 'react';
+
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    'PlusJakartaSnas-Bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'PlusJakartaSnas-Medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'PlusJakartaSnas-SemiBold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='(onboarding)' options={{ headerShown: false }} />
+      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+      <Stack.Screen name='(root)' options={{ headerShown: false }} />
+    </Stack>
+  );
 }
