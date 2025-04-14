f78e1319dd90865ea6821066c2dcfebc5fd46dd9
Khoa1512 <danggkhoaa1512@gmail.com>
2025-04-14 11:17:14 +0700

    config icons and images

---
+++ constants/icons.ts
@@ -0,0 +1,64 @@
+import google from "@/assets/icons/google.jpg";
+import apple from "@/assets/icons/apple.jpg";
+import backArrow from "@/assets/icons/back-arrow.png";
+import home from "@/assets/icons/home.png";
+import homefocused from "@/assets/icons/homefocused.png";
+import user from "@/assets/icons/user.png";
+import userfocused from "@/assets/icons/userfocused.png";
+import calendar from "@/assets/icons/calendar.png";
+import calendarfocused from "@/assets/icons/calendarfocused.png";
+import document from "@/assets/icons/document.png";
+import documentfocused from "@/assets/icons/documentfocused.png";
+import plus from "@/assets/icons/plus.png";
+import edit from "@/assets/icons/edit.png";
+import rightarrow from "@/assets/icons/rightarrow.png";
+import profile from "@/assets/icons/profile.png";
+import lock from "@/assets/icons/lock.png";
+import unlock from "@/assets/icons/unlock.png";
+import shielddone from "@/assets/icons/shielddone.png";
+import shield from "@/assets/icons/shield.png";
+import globe from "@/assets/icons/globe.png";
+import trash from "@/assets/icons/trash.png";
+import layer from "@/assets/icons/layer.png";
+import activity from "@/assets/icons/activity.png";
+import edit1 from "@/assets/icons/edit1.png";
+import camera from "@/assets/icons/camera.png";
+import frame from "@/assets/icons/frame.png";
+import trashred from "@/assets/icons/trashred.png";
+
+
+
+
+
+
+
+
+export default {
+  google,
+  apple,
+  backArrow,
+  home,
+  homefocused,
+  user,
+  userfocused,
+  calendar,
+  calendarfocused,
+  document,
+  documentfocused,
+  plus,
+  edit,
+  rightarrow,
+  profile,
+  lock,
+  unlock,
+  shielddone,
+  shield,
+  trash,
+  layer,
+  globe,
+  activity,
+  edit1,
+  camera,
+  trashred,
+  frame
+};
---
+++ constants/images.ts
@@ -0,0 +1,24 @@
+import onboarding1 from '@/assets/images/onboarding1.jpg';
+import onboarding2 from "@/assets/images/onboarding2.jpg";
+import onboarding3 from "@/assets/images/onboarding3.jpg";
+import onboarding4 from "@/assets/images/onboarding4.jpg";
+import avatar from "@/assets/images/avatar.png";
+import backgroudprofile from "@/assets/images/backgroundprofile.png";
+import avatarprofile from "@/assets/images/avatarprofile.png";
+import map from "@/assets/images/map.png";
+
+
+
+
+
+
+export default {
+  onboarding1,
+  onboarding2,
+  onboarding3,
+  onboarding4,
+  avatar,
+  backgroudprofile,
+  avatarprofile,
+  map
+};
